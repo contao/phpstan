270419d754692dd9087bff001416381472785a84
David Greminger <david.greminger@1up.io>
2018-07-25 09:36:51 +0200

    Fix ContaoTestCase::mockClassWithProperties return type

--- src/Type/Contao/ContaoFrameworkDynamicMethodReturnTypeExtension.php
+++ src/Type/Contao/ContaoFrameworkDynamicMethodReturnTypeExtension.php
@@ -5,9 +5,16 @@ declare(strict_types=1);
 namespace Oneup\PHPStan\Type\Contao;
 
 use Contao\CoreBundle\Framework\ContaoFramework;
+use PhpParser\Node\Arg;
+use PhpParser\Node\Expr\ClassConstFetch;
+use PhpParser\Node\Expr\MethodCall;
+use PHPStan\Analyser\Scope;
 use PHPStan\Reflection\MethodReflection;
+use PHPStan\Type\DynamicMethodReturnTypeExtension;
+use PHPStan\Type\ObjectType;
+use PHPStan\Type\Type;
 
-class ContaoFrameworkDynamicMethodReturnTypeExtension extends AbstractDynamicMethodReturnTypeFromMethodCallExtension
+class ContaoFrameworkDynamicMethodReturnTypeExtension implements DynamicMethodReturnTypeExtension
 {
     public function getClass(): string
     {
@@ -17,5 +24,21 @@ class ContaoFrameworkDynamicMethodReturnTypeExtension extends AbstractDynamicMet
     public function isMethodSupported(MethodReflection $methodReflection): bool
     {
         return 'createInstance' === $methodReflection->getName();
+    }
+
+    public function getTypeFromMethodCall(MethodReflection $methodReflection, MethodCall $methodCall, Scope $scope): Type
+    {
+        /** @var Arg $argument */
+        foreach ($methodCall->args as $argument) {
+            $value = $argument->value;
+
+            if ($value instanceof ClassConstFetch) {
+                return new ObjectType((string) $value->class);
+            }
+        }
+
+        $arg = $methodCall->args[0]->value;
+
+        return $scope->getType($arg);
     }
 }
--- src/Type/Contao/ContaoTestCaseDynamicMethodReturnTypeExtension.php
+++ src/Type/Contao/ContaoTestCaseDynamicMethodReturnTypeExtension.php
@@ -5,9 +5,18 @@ declare(strict_types=1);
 namespace Oneup\PHPStan\Type\Contao;
 
 use Contao\TestCase\ContaoTestCase;
+use PhpParser\Node\Arg;
+use PhpParser\Node\Expr\ClassConstFetch;
+use PhpParser\Node\Expr\MethodCall;
+use PHPStan\Analyser\Scope;
 use PHPStan\Reflection\MethodReflection;
+use PHPStan\Type\DynamicMethodReturnTypeExtension;
+use PHPStan\Type\IntersectionType;
+use PHPStan\Type\ObjectType;
+use PHPStan\Type\Type;
+use PHPUnit\Framework\MockObject\MockObject;
 
-class ContaoTestCaseDynamicMethodReturnTypeExtension extends AbstractDynamicMethodReturnTypeFromMethodCallExtension
+class ContaoTestCaseDynamicMethodReturnTypeExtension implements DynamicMethodReturnTypeExtension
 {
     public function getClass(): string
     {
@@ -17,5 +26,24 @@ class ContaoTestCaseDynamicMethodReturnTypeExtension extends AbstractDynamicMeth
     public function isMethodSupported(MethodReflection $methodReflection): bool
     {
         return 'mockClassWithProperties' === $methodReflection->getName();
+    }
+
+    public function getTypeFromMethodCall(MethodReflection $methodReflection, MethodCall $methodCall, Scope $scope): Type
+    {
+        /** @var Arg $argument */
+        foreach ($methodCall->args as $argument) {
+            $value = $argument->value;
+
+            if ($value instanceof ClassConstFetch) {
+                return new IntersectionType([
+                    new ObjectType((string) $value->class),
+                    new ObjectType(MockObject::class),
+                ]);
+            }
+        }
+
+        $arg = $methodCall->args[0]->value;
+
+        return $scope->getType($arg);
     }
 }
