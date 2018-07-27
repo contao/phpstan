d867901f8b72964557911dc67f6d9fc5bfc72d85
David Greminger <david.greminger@1up.io>
2018-07-27 17:36:49 +0200

    More tests

---
+++ tests/Contao/ServiceTest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Oneup\PHPStan\Tests\Contao;
+
+use Oneup\PHPStan\Contao\Service;
+use PHPUnit\Framework\TestCase;
+
+class ServiceTest extends TestCase
+{
+    private $service;
+
+    public function setUp()/* The :void return type declaration that should be here would cause a BC issue */
+    {
+        $this->service = new Service(
+            'service.id',
+            null,
+            true,
+            false,
+            'service.alias',
+            false
+        );
+    }
+
+    public function testCanBeInstantiated(): void
+    {
+        $this->assertInstanceOf('Oneup\PHPStan\Contao\Service', $this->service);
+    }
+
+    public function testReturnsValidId(): void
+    {
+        $this->assertSame('service.id', $this->service->getId());
+    }
+
+    public function testReturnsValidClass(): void
+    {
+        $this->assertNull($this->service->getClass());
+    }
+
+    public function testIsPublic(): void
+    {
+        $this->assertTrue($this->service->isPublic());
+    }
+
+    public function testIsSynthetic(): void
+    {
+        $this->assertFalse($this->service->isSynthetic());
+    }
+
+    public function testReturnsValidAlias(): void
+    {
+        $this->assertSame('service.alias', $this->service->getAlias());
+    }
+
+    public function testIsHidden(): void
+    {
+        $this->assertFalse($this->service->isHidden());
+    }
+}
