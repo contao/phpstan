6fcbc29e2fbf5b2c2076112043f8459d8d4effac
Andreas Schempp <andreas.schempp@terminal42.ch>
2019-06-11 18:38:32 +0200

    Use the core-bundle functional tests for phpstan runtime

--- src/AppKernel.php
+++ src/AppKernel.php
@@ -12,68 +12,12 @@ declare(strict_types=1);
 
 namespace Contao\PhpStan;
 
-use Contao\CoreBundle\ContaoCoreBundle;
-use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
-use Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle;
-use FOS\HttpCacheBundle\FOSHttpCacheBundle;
-use Knp\Bundle\MenuBundle\KnpMenuBundle;
-use Knp\Bundle\TimeBundle\KnpTimeBundle;
-use Lexik\Bundle\MaintenanceBundle\LexikMaintenanceBundle;
-use Nelmio\CorsBundle\NelmioCorsBundle;
-use Scheb\TwoFactorBundle\SchebTwoFactorBundle;
-use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
-use Symfony\Bundle\MonologBundle\MonologBundle;
-use Symfony\Bundle\SecurityBundle\SecurityBundle;
-use Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle;
-use Symfony\Bundle\TwigBundle\TwigBundle;
 use Symfony\Component\Config\ConfigCache;
-use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Dumper\XmlDumper;
-use Symfony\Component\HttpKernel\Kernel;
 
-class AppKernel extends Kernel
+class AppKernel extends \Contao\CoreBundle\Tests\Functional\app\AppKernel
 {
-    public function registerBundles()
-    {
-        return [
-            new FrameworkBundle(),
-            new SecurityBundle(),
-            new TwigBundle(),
-            new MonologBundle(),
-            new SwiftmailerBundle(),
-            new DoctrineBundle(),
-            new DoctrineCacheBundle(),
-            new LexikMaintenanceBundle(),
-            new NelmioCorsBundle(),
-            new FOSHttpCacheBundle(),
-            new SchebTwoFactorBundle(),
-            new KnpTimeBundle(),
-            new KnpMenuBundle(),
-            new ContaoCoreBundle(),
-        ];
-    }
-
-    public function getRootDir()
-    {
-        return __DIR__;
-    }
-
-    public function getCacheDir()
-    {
-        return \dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
-    }
-
-    public function getLogDir()
-    {
-        return \dirname(__DIR__).'/var/logs';
-    }
-
-    public function registerContainerConfiguration(LoaderInterface $loader): void
-    {
-        $loader->load($this->getRootDir().'/Resources/config/config_'.$this->getEnvironment().'.yml');
-    }
-
     public function dumpContainer(ConfigCache $cache, ContainerBuilder $container, $class, $baseClass): void
     {
         parent::dumpContainer($cache, $container, $class, $baseClass);
