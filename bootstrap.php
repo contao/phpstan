c1dafe0975899353f80b35069b1550558e60ae7d
Leo Feyer <github@contao.org>
2020-03-02 14:16:30 +0100

    Define the constants in a bootstrap file

---
+++ bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+// Compatibility with PhpStan 0.12.12 (see https://github.com/contao/contao/pull/1451)
+define('TL_MODE', 'BE');
+define('TL_START', 0);
+define('TL_ROOT', __DIR__);
+define('TL_REFERER_ID', '');
+define('TL_SCRIPT', '');
+define('BE_USER_LOGGED_IN', false);
+define('FE_USER_LOGGED_IN', false);
+define('TL_PATH', '/');
